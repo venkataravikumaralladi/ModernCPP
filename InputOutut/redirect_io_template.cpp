ef7b955c3622544e978d6bfae94ed804cca57551
Venkata Ravi K A <venkataravikumaralladi@gmail.com>
2022-01-27 12:04:16 +0530

    updated redirect_io_template.cpp

--- InputOutut/redirect_io_template.cpp
+++ InputOutut/redirect_io_template.cpp
@@ -13,24 +13,66 @@ using namespace std;
 
 int main() {
 	 
-	fstream file;
-	file.open("cout.txt", ios::out);
-	string line;
+	fstream output_file;
+	output_file.open("cout.txt", ios::out);
+	fstream input_file;
+	input_file.open("cin.txt", ios::in);
 
 	// backup existing stream buffers 
 	streambuf* cin_old_streambuf = cin.rdbuf();
 	streambuf* cout_old_streambuf = cout.rdbuf();
 
-	// Get stream buffer of file and redirect to cout
-	streambuf* file_streambuf = file.rdbuf();
-	cout.rdbuf(file_streambuf);
+	// Get output stream buffer of file and redirect to cout
+	streambuf* output_file_streambuf = output_file.rdbuf();
+	cout.rdbuf(output_file_streambuf);
 
-	/* What ever you do with cout will write to file. */
+	// Get input stream buffer of file and redirect to cin
+	streambuf* input_file_streambuf = input_file.rdbuf();
+	cin.rdbuf(input_file_streambuf);
 
-	// Redirect cout to default console.
+
+	/* What ever you do with cin and cout will write to file. */
+	/* Scenario 1: using getline to read data from input stream */
+	string line;
+	getline(cin, line); // Note: getline will read \n but it won't print/write to string or terminal.
+	cout << line;
+	getline(cin, line);
+	cout << line;
+	getline(cin, line);
+	cout << line;
+	getline(cin, line);
+	cout << line;
+	/* Scenario 1 output of cout file looks like below with above code
+	   My name is raviMy age is 45
+	*/
+
+	/*
+	 Scenario 2: using cin to read data from input stream
+	*/
+	/*
+	string line;
+	cin >> line;
+	cout << line;
+	cin >> line;
+	cout << line;
+	cin >> line;
+	cout << line;
+
+	cout << line;
+	cin >> line;
+	cout << line;
+	cin >> line;
+	cout << line;
+	*/
+	/*
+		Scenario 2: output of cout file looks like below with above code
+					MynameisisraviMy
+	*/
+
+	// Redirect cin and cout to default.
 	cout.rdbuf(cout_old_streambuf);
-	cout << "printing to default output console " << endl;
+	cin.rdbuf(cin_old_streambuf);
+	cout << "printing output console " << endl;
 
-	file.close();
-	return 0;
+	output_file.close();
 }
